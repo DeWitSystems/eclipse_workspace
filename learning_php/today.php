387c46abefeb8a151b7b15b55e04c24658de4b9d
iphipan <50053458+iphipan@users.noreply.github.com>
2019-06-29 13:36:19 +0200

    Add files via upload

---
+++ learning_php/today.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	<meta charset="utf-8">
+	<title>Today&rsquo;s Date</title>
+	</head>
+	<body>
+	<p>Today&rsquo;s date (according to this webserver) is
+	<?php 
+	
+	echo date('1, F jS Y.');
+	
+	
+	?>
+	</body>
+	 </html>
