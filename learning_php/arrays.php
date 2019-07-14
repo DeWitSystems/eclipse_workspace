8236b209d9a501cff8d813714c3a9e06000af01f
zirkphex <50053458+zirkphex@users.noreply.github.com>
2019-07-14 20:20:42 +0200

    Add files via upload

---
+++ learning_php/arrays.php
@@ -0,0 +1,8 @@
+<?php
+    $myinfo = array("John", 45, "USA", "College");
+
+    echo "My name is " . $myinfo[0] . ".\n";
+    echo "I am " . $myinfo[1] . " years old. \n";
+    echo "I live in " . $myinfo[2] . ".\n";
+    echo "My latest education level is " . $myinfo[3] .".\n";
+ ?>
